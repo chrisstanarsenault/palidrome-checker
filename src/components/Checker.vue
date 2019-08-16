d733663c046636a828b3b19b8b80043ad947384c
Chris Arsenault <chrisstanarsenault@gmail.com>
2019-08-16 00:42:43 -0400

    added logic to catch if no words were entered with error message.  Added palidrome word highlighting.

--- src/components/Checker.vue
+++ src/components/Checker.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="full-container">
-  <div id="palindrome-wrapper">
-    <div id="form-container">
-      <form id="message" v-on:submit.prevent="palindromeCheck">
-        <input v-model.lazy="string" id="string" name="string" placeholder="Enter a word or sentence" />
-        <button>Submit</button>
-        </form>
-      </div>
-      <span id="answer" transition=fade>{{palindrome}}{{palindromeSentence}}</span>
-  </div>
+    <div id="palindrome-wrapper">
+      <div id="form-container">
+        <form id="message" v-on:submit.prevent="palindromeCheck">
+          <input v-model.lazy="string" id="string" name="string" placeholder="Enter a word or sentence" />
+          <button>Submit</button>
+          </form>
+        </div>
+        <transition name="fade">
+          <span v-if="true" id="answer"><span id="highlight" class="highlight">{{palindrome}}</span>{{palindromeSentence}}</span>
+        </transition>
+    </div>
   </div>
 </template>
 
@@ -21,7 +23,8 @@ export default {
       palindrome: "",
       palindromePass: false,
       isTrue: false,
-      palindromeSentence: "dont read me"
+      palindromeSentence: "if you can see me, we have a problem here",
+      wordsThatArePalindromes: []
     }
   },
   methods: {
@@ -29,17 +32,25 @@ export default {
       const stringArray = this.string.toLowerCase().split("");
       const mapString1 = stringArray.map(s => s).join().replace(/[^A-Za-z0-9]+/g, "");
       const mapString2 = stringArray.reverse().map(s => s).join().replace(/[^A-Za-z0-9]+/g, "");
+      let element = document.getElementById("highlight");
 
-      if (mapString1 == mapString2) {
+      if (stringArray == "") {
+        this.palindrome = "";
+        this.palindromeSentence = "You didnt even enter anything!";
+        element.classList.remove("highlight");
+      } else if (mapString1 == mapString2) {
         this.palindrome = this.string
         this.palindromePass = true;
         this.isTrue = true;
-        this.palindromeSentence = " is a palindrome!"
+        this.palindromeSentence = " is a palindrome!";
+        this.wordsThatArePalindromes.push(this.palindrome);
+        element.className="highlight";
       } else {
         this.palindrome = this.string
         this.palindromePass = false;
         this.isTrue = false;
         this.palindromeSentence = " is NOT a palindrome!"
+        element.className="highlight";
       }
       document.getElementById("answer").style.visibility = "visible"
       this.string="";
@@ -98,9 +109,23 @@ export default {
     border: 1px solid green;
   }
 
-  span {
+  #answer {
     margin-top: 20px;
     font-size: 30px;
     visibility: hidden;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: all 0.3s ease;
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+    transform: scale(0);
+  }
+
+  .highlight {
+    background-color: yellow;
+    padding: 0 10px;
   }
 </style>
